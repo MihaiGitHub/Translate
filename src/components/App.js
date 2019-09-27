e49b8809cd1f1908dbe34c4690955458c68bc4e6
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-26 22:21:58 -0700

    Create language selector

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {
+        language: 'english'
+    }
+
+    // Define as an arrow function because it is a callback method that we are passing to onClick
+    onLanguageChange = language => {
+        this.setState({
+            language
+        });
+    }
+
     render(){
-        return <div className="ui container">App</div>;
+        return (
+            <div className="ui container">
+                <div>Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+                </div>
+                {this.state.language}
+            </div>
+        );
     }
 }
 
