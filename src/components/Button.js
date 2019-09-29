221f1fd4a84de395578f0c20228a7f8f086acc03
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-29 16:50:23 -0700

    Get data out of context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+    // Define contextType using the context object; Name needs to be contextType
+    static contextType = LanguageContext;
+
     render(){
+        console.log(this.context)
         return <button className="ui button primary">Submit</button>;
     }
 }
