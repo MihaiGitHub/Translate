1523ec79872ff5f098c06106f56dbe86c3439081
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-30 13:37:20 -0700

    Pull data from multiple contexts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
     state = {
@@ -21,12 +22,15 @@ class App extends React.Component {
                     <i className="flag us" onClick={() => this.onLanguageChange('english')} />
                     <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
                 </div>
+            <ColorContext.Provider value="red">
                 {/* Communicate information from this component into the context object
                     Value to put inside context object; language */}
                 <LanguageContext.Provider value={this.state.language}>
-                    {/* UserCreate needs to know some info from the context object; language */}
+                    {/* UserCreate needs to know some info from the context object; language;
+                        UserCreate needs to be wrapped with all context providers from where it needs data */}
                     <UserCreate />
                 </LanguageContext.Provider>
+            </ColorContext.Provider>
             </div>
         );
     }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
     renderSubmit(value){
         return value === 'english' ? 'Submit' : 'Voorleggen'; 
     }
 
-    render(){
+    renderButton(color){
         return (
-            <button className="ui button primary">
+            <button className={`ui button ${color}`}>
                 {/* Consumer component to get data out of context object 
                     Pass a child (argument) into the cosumer; The function will be called by the consumer
                     with the value that is inside the pipe; value is the current value inside the pipe */}
@@ -16,6 +17,15 @@ class Button extends React.Component {
                     {value => this.renderSubmit(value)}
                 </LanguageContext.Consumer>
             </button>
+        );
+    }
+
+    /* Pass function to color context and return entire block of JSX; All consumers need a () => as argument */
+    render(){
+        return (
+        <ColorContext.Consumer>
+            {color => this.renderButton(color)}
+        </ColorContext.Consumer>
         );
     }
 }
