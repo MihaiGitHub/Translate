781d1cc10bd2f3684d937483ee87ce4108e876b0
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-09-30 18:24:29 -0700

    Create the Language Store

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,26 @@
+// Contains all business logic and data and make it available to all other components
 import React from 'react';
+import LanguageSelector from '../components/LanguageSelector';
 
-export default React.createContext('english');
+// Capital C required
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+    state = { language: 'english' };
+
+    // Give nested components the ability to change language
+    onLanguageChange = (language) => {
+        this.setState({ language });
+    };
+
+    // Value will be all the data and the ability to change that data
+    render(){
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+}
+
+export default Context;
